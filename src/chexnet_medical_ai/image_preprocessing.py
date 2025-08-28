96d6769d978b745138e97ae8664187f0bfe313f2
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-08-28 21:51:48 +0530

    skeleton class for image transformations

--- src/chexnet_medical_ai/image_preprocessing.py
+++ src/chexnet_medical_ai/image_preprocessing.py
@@ -1,8 +1,18 @@
 """module for image preprocessing"""
 
+from dataclasses import dataclass
 import torch
 from torch import nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms, models
 from torchvision.utils import make_grid
+
+@dataclass
+class ImageTransForm:
+    """train transformation"""
+
+    train_transforms: transforms.Compose
+
+    def train_image_transforms(self):
+        pass
