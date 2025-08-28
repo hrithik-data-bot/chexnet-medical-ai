b95cfe501caae8faff348ccae6eeca2305982e50
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-08-28 21:58:49 +0530

    completed train transformations

--- src/chexnet_medical_ai/image_preprocessing.py
+++ src/chexnet_medical_ai/image_preprocessing.py
@@ -13,6 +13,23 @@ class ImageTransForm:
     """train transformation"""
 
     train_transforms: transforms.Compose
+    test_transforms: transforms.Compose
 
     def train_image_transforms(self):
+        """train image transformations"""
+
+        self.train_transforms = transforms.Compose([
+            transforms.RandomRotation(10),
+            transforms.RandomHorizontalFlip(),
+            transforms.Resize(224),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406, 
+                                  0.229, 0.224, 0.225])
+            
+        ])
+
+
+    def test_image_transforms(self):
+        """test image transformations"""
         pass
