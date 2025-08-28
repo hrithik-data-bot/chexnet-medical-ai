c850ab382d82dd0ff4862d5c022f55a2cb89c715
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-08-28 22:04:36 +0530

    added test transformation

--- src/chexnet_medical_ai/image_preprocessing.py
+++ src/chexnet_medical_ai/image_preprocessing.py
@@ -1,12 +1,7 @@
 """module for image preprocessing"""
 
 from dataclasses import dataclass
-import torch
-from torch import nn as nn
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms, models
-from torchvision.utils import make_grid
+from torchvision import transforms
 
 @dataclass
 class ImageTransForm:
@@ -15,7 +10,7 @@ class ImageTransForm:
     train_transforms: transforms.Compose
     test_transforms: transforms.Compose
 
-    def train_image_transforms(self):
+    def train_image_transforms(self) -> transforms.Compose:
         """train image transformations"""
 
         self.train_transforms = transforms.Compose([
@@ -28,8 +23,16 @@ class ImageTransForm:
                                   0.229, 0.224, 0.225])
             
         ])
+        return self.train_transforms
 
-
-    def test_image_transforms(self):
+    def test_image_transforms(self) -> transforms.Compose:
         """test image transformations"""
-        pass
+
+        self.test_transforms = transforms.Compose([
+            transforms.Resize(224),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406, 
+                                  0.229, 0.224, 0.225])
+        ])
+        return self.test_transforms
