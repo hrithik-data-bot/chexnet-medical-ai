eec26618861cb487f7b4b2862e484c8a1c3f32a2
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-08-29 22:01:29 +0530

    completed model class

--- src/chexnet_medical_ai/model.py
+++ src/chexnet_medical_ai/model.py
@@ -1,6 +1,7 @@
 """Convolutional Neutal Network Classifier"""
 
 from torch import nn as nn
+from torch.nn import functional as F
 
 class ConvolutionalNewtwork(nn.Module):
     """CNN Class"""
@@ -14,5 +15,18 @@ class ConvolutionalNewtwork(nn.Module):
         self.fc1 = nn.Linear(54*54*16, 120)
         self.fc2 = nn.Linear(120,84)
         self.fc3 = nn.Linear(84,2)
+
+
+    def forward(self, X):
+        """forward propagation"""
         
+        X = F.relu(self, self.conv1(X))
+        X = F.max_pool2d(X,2,2)
+        X = F.relu(self.conv2(X))
+        X = F.max_pool2d(X,2,2)
+        X = X.view(-1,54*54*16)
+        X = F.relu(self.fc1(X))
+        X = F.relu(self.fc2(X))
+        X = self.fc3(X)
+        return F.log_softmax(X, dim=1)
         
