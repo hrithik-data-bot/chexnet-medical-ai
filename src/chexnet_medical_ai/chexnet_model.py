27120096c8fe082fdb2ab36d9d6783c139f073e1
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-08-30 16:18:49 +0530

    model as paper

--- src/chexnet_medical_ai/chexnet_model.py
+++ src/chexnet_medical_ai/chexnet_model.py
@@ -4,3 +4,87 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from torch import functional as F
+
+class _DenseLayer(nn.Module):
+    
+    def __init__(self, in_channels, growth_rate, bn_size=4, drop_rate=0.0):
+        super(_DenseLayer, self).__init__()
+        self.norm1 = nn.BatchNorm2d(in_channels)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(in_channels, bn_size * growth_rate,
+                               kernel_size=1, stride=1, bias=False)
+
+        self.norm2 = nn.BatchNorm2d(bn_size * growth_rate)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(bn_size * growth_rate, growth_rate,
+                               kernel_size=3, stride=1, padding=1, bias=False)
+
+        self.drop_rate = drop_rate
+
+
+    def forward(self, x):
+        out = self.conv1(self.relu1(self.norm1(x)))
+        out = self.conv2(self.relu2(self.norm2(out)))
+        if self.drop_rate > 0:
+            out = F.dropout(out, p=self.drop_rate, training=self.training)
+        return torch.cat([x, out], 1)
+
+class _DenseBlock(nn.Module):
+    def __init__(self, num_layers, in_channels, growth_rate, bn_size=4, drop_rate=0.0):
+        super(_DenseBlock, self).__init__()
+        layers = []
+        for i in range(num_layers):
+            layers.append(
+                _DenseLayer(in_channels + i * growth_rate, growth_rate, bn_size, drop_rate)
+            )
+        self.block = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.block(x)
+
+class _Transition(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(_Transition, self).__init__()
+        self.norm = nn.BatchNorm2d(in_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv = nn.Conv2d(in_channels, out_channels,
+                              kernel_size=1, stride=1, bias=False)
+        self.pool = nn.AvgPool2d(kernel_size=2, stride=2)
+
+    def forward(self, x):
+        x = self.conv(self.relu(self.norm(x)))
+        x = self.pool(x)
+        return x
+
+class DenseNet121(nn.Module):
+    def __init__(self, growth_rate=32, block_config=(6,12,24,16),
+                 num_init_features=64, bn_size=4, drop_rate=0.0, num_classes=2):
+        super(DenseNet121, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, num_init_features, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(num_init_features),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            block = _DenseBlock(num_layers, num_features, growth_rate, bn_size, drop_rate)
+            self.features.add_module(f'denseblock{i+1}', block)
+            num_features = num_features + num_layers * growth_rate
+            if i != len(block_config) - 1:
+                trans = _Transition(num_features, num_features // 2)
+                self.features.add_module(f'transition{i+1}', trans)
+                num_features = num_features // 2
+
+        self.features.add_module('norm5', nn.BatchNorm2d(num_features))
+        self.classifier = nn.Linear(num_features, num_classes)
+
+    def forward(self, x):
+        features = self.features(x)
+        out = F.relu(features, inplace=True)
+        out = F.adaptive_avg_pool2d(out, (1,1)).view(features.size(0), -1)
+        out = self.classifier(out)
+        return out
